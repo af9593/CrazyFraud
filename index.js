08a75bbb3852d22ab520d5bbd844713b6e6d4eaa
chungpaella <48243075+chungpaella@users.noreply.github.com>
2020-10-02 12:15:38 +0200

    knapp på js

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("myBtn").addEventListener("click", function(){
+
+    var newAddress = prompt("Change Address")
+    var newNumber = prompt("Change Number")
+    var newEmail = prompt("Change Email")
+
+    if(newAddress){
+        document.getElementById("profil_info").getElementsByTagName("p")[0].innerText = newAddress;
+    }
+    if(newNumber){    
+        document.getElementById("profil_info").getElementsByTagName("p")[1].innerText = newNumber;
+            
+    }
+    if(newEmail){
+        document.getElementById("profil_info").getElementsByTagName("p")[2].innerText = newEmail;
+
+    }
+
+});
